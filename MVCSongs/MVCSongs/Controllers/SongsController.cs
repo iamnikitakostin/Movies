f5668b08d971616af3b8f7441d3b3f97423163d3
iamnikitakostin <contact@nikitakostin.com>
2025-02-23 12:47:57 -0800

    finished the project

---
+++ MVCSongs/MVCSongs/Controllers/SongsController.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MVCSongs.Data;
+using MVCSongs.Models;
+
+namespace MVCSongs.Controllers
+{
+    public class SongsController : Controller
+    {
+        private readonly MVCSongsContext _context;
+
+        public SongsController(MVCSongsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Songs
+        public async Task<IActionResult> Index(string songGenre, string searchQuery)
+        {
+            if (_context.Song == null)
+            {
+                return Problem("Entity set 'MvcSongContext.Song' is null.");
+            }
+
+            IQueryable<string> genresQuery = _context.Song.OrderBy(s => s.Genre).Select(s => s.Genre);
+
+            var songs = _context.Song.ToList();
+
+            if (!String.IsNullOrEmpty(searchQuery)) {
+                songs = songs.Where(s => s.Name!.ToUpper().Contains(searchQuery.ToUpper()) || s.Author!.ToUpper().Contains(searchQuery.ToUpper())).ToList();
+            }
+
+            if (!String.IsNullOrEmpty(songGenre))
+            {
+                songs = songs.Where(s => s.Genre == songGenre).ToList();
+            }
+
+            var songGenreViewModel = new SongGenreViewModel
+            {
+                Genres = new SelectList(await genresQuery.Distinct().ToListAsync()),
+                Songs = songs
+            };
+
+            return View(songGenreViewModel);
+        }
+
+        // GET: Songs/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Song
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // GET: Songs/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Songs/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Name,Author,ReleaseDate,Genre,Price,TimesListened")] Song song)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(song);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Song.FindAsync(id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            return View(song);
+        }
+
+        // POST: Songs/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Author,ReleaseDate,Genre,Price,TimesListened")] Song song)
+        {
+            if (id != song.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(song);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!SongExists(song.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(song);
+        }
+
+        // GET: Songs/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Song
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+
+            return View(song);
+        }
+
+        // POST: Songs/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var song = await _context.Song.FindAsync(id);
+            if (song != null)
+            {
+                _context.Song.Remove(song);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool SongExists(int id)
+        {
+            return _context.Song.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ MVCSongs/MVCSongs/Data/MVCSongsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVCSongs.Models;
+
+namespace MVCSongs.Data
+{
+    public class MVCSongsContext : DbContext
+    {
+        public MVCSongsContext (DbContextOptions<MVCSongsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVCSongs.Models.Song> Song { get; set; } = default!;
+    }
+}
